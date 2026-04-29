f9d2c2cd8fa4a5ec0c6ef8fcc255669a7ef98915
steeldeezer-web <steel.deezer@gmail.com>
2026-04-29 23:05:22 +0500

    Решена проблема гонки потоков  разным способами  в пакете MultiThread

---
+++ OOMTEST/src/MultiThread/Counter2.java
@@ -0,0 +1,32 @@
+package MultiThread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter2 {
+    private static int count =  0;
+
+    public synchronized static void increment() {
+        count++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    increment();
+                }
+            });
+            threads.add(t);
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        System.out.println("Expected: 100000, Got: " + count);
+    }
+}
